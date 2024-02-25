ec689e2c797535024fb7a4363b61c5d0d74f82f6
Sir Orison <ansreorison@gmail.com>
2024-02-25 06:23:18 +0000

    local commit firsrt

---
+++ DatingApp/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using DatingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) 
+        { 
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    if (!optionsBuilder.IsConfigured)
+        //    {
+        //        optionsBuilder.UseSqlite("ConnectionString");
+        //    }
+        //}
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
